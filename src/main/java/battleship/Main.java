2ea22c8c0a1b69acae403b8e36c596635982d005
Diogo Soares <diogopsoares91@gmail.com>
2026-03-22 12:24:26 +0000

    #21 pedir nome do jogador no arranque da sessão

--- src/main/java/battleship/Main.java
+++ src/main/java/battleship/Main.java
@@ -19,7 +19,7 @@ public class Main
 	 */
 	public static void main(String[] args)
     {
-		System.out.println("***  Battleship  ***");
+		//System.out.println("***  Battleship  ***");
 
 		Tasks.menu();
     }
