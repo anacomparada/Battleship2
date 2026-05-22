edf88c78afb418deaabb6f9d96437cc5f2e6682e
Diogo Soares <dpsss@iscte-iul.pt>
2026-05-22 19:00:05 +0100

    Rename logger variable to LOGGER

--- src/main/java/battleship/Tasks.java
+++ src/main/java/battleship/Tasks.java
@@ -19,7 +19,7 @@ public class Tasks {
      * The constant LOGGER.
      */
     //private static final Logger LOGGER = LogManager.getLogger();
-    private static final Logger logger = LogManager.getLogger(Tasks.class);
+    private static final Logger LOGGER = LogManager.getLogger(Tasks.class);
     /**
      * The constant GOODBYE_MESSAGE.
      */
